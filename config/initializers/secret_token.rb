c4669e12aef3cbfb88f0370026b27a3522ac4bf4
Damian C. Williams <damian@askagp.com>
2013-03-10 13:23:44 +0000

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ptrecord::Application.config.secret_token = 'c5e4a094279f179f9acd885aabb08c6a63b64a45107ea21d5a476cdadd5951d879dd5f61b35d312f5a757c3690f022575afcebd13e785e1edfed7ccff3a2f4ce'
